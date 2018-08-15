b94f60e97c590c925772a5e3e6ddbe4df6917e16
Karim Aouadi <kdolphinus@gmail.com>
2018-08-15 19:36:01 +0200

    Added tutorial-4

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,17 +3,27 @@
 
 <div class="container">
 
-    <h4>Buttons</h4>
-    <a href="#" class="btn waves-effect waves-dark">Normal Button</a>
-    <a href="#" class="btn-small">Small button</a>
-    <a href="#" class="btn-large">Large button</a>
-    <a href="#" class="btn indigo waves-effect waves-light">Large button</a>
-    <a href="#" class="btn disabled">disable button</a>
-    <h4>Floating button </h4>
-    <a href="#" class="btn-floating pink pulse"></a>
-    <h4>Other element</h4>
-     <a href="#" class="btn">I m a div</a>
-
+    <h4>Icons</h4>
+    <i class="material-icons red-text">error</i>
+    <i class="material-icons yellow-text text-darken-2">warning</i>
+    <i class="material-icons grey-text">cloud</i>
+    <i class="material-icons blue-text">folder</i>
+    <h4>Icons on button</h4>
+    <a href="#" class="btn indigo waves-effect waves-light">
+        <span>Send</span>
+        <i class="material-icons  right">send</i>
+    </a>
+    <a href="#" class="btn pink waves-effect waves-light">
+        <span>email</span>
+        <i class="material-icons  right">email</i>
+    </a>
+     <h4>Icons pulse</h4>
+    <a href="#" class="btn-floating blue pulse">
+        <i class="material-icons ">add</i>
+    </a>
+    <a href="#" class="btn-floating pink pulse">
+        <i class="material-icons ">remove</i>
+    </a>
 
 </div>
     
