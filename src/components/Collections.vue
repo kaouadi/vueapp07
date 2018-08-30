edf8bf7f090e6c14037352c9224de65cdb08ead0
Karim Aouadi <kdolphinus@gmail.com>
2018-08-30 17:40:24 +0200

    Added tutorial-10

---
+++ src/components/Collections.vue
@@ -0,0 +1,34 @@
+<template>
+<div class="container">
+    <h2>Collections</h2>
+    <ul class="collection with-header">
+        <li class="collection-header"><h5>Customers</h5></li>
+        <li class="collection-item avatar">
+            <i class="material-icons circle blue">person</i>
+            <span class="title">Ryu</span>
+            <p class="grey-text">black-belt</p>
+            <a href="#" class="secondary-content">
+                <i class="material-icons blue-text">email</i>
+            </a>
+        </li>
+        <li class="collection-item">Yoshi</li>
+        <li class="collection-item">Chrystal</li>
+        <li class="collection-item">Mario</li>
+        <li class="collection-item">Luigi</li>
+        <li class="collection-item">Bowser</li>
+    </ul>
+
+</div>
+    
+</template>
+
+<script>
+export default {
+    name: 'Collections'
+
+}
+</script>
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
 import Cards from '@/components/Cards'
+import Collections from '@/components/Collections'
 
 
 Vue.use(Router)
@@ -17,6 +18,11 @@ const router = new Router({
       path: '/cards',
       name: 'Cards',
       component: Cards
+    },
+    {
+      path: '/collections',
+      name: 'Collections',
+      component: Collections
     }
 ]
 })
