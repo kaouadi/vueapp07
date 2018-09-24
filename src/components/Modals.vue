60ec2c89c6684ab2113acb6fa651e4652a90b791
Karim Aouadi <kdolphinus@gmail.com>
2018-09-24 16:49:58 +0200

    fix bug modal

--- src/components/Modals.vue
+++ src/components/Modals.vue
@@ -2,21 +2,22 @@
 
 <div class="container">
     <h2>Modals</h2>
-    <a href="#terms" class="btn orange modal-trigers">Terms & conditions</a>
 
-    <div class="modal" id="terms">
+    <!-- Modal Trigger -->
+    <a class="waves-effect waves-light btn orange modal-trigger" href="#modal1">Modal</a>
+
+    <!-- Modal Structure -->
+    <div id="modal1" class="modal">
         <div class="modal-content">
-             <p>
+            <h4>Modal Header</h4>
+            <p>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
                 Sed non risus. Suspendisse lectus tortor, dignissim sit amet,
                 Sed non risus. Suspendisse lectus tortor, dignissim sit amet,
             </p>
-
         </div>
         <div class="modal-footer">
-            <a href="#" class="modal-close btn orange">
-                Agree & Close
-            </a>
+            <a href="#!" class="modal-close waves-effect waves-green btn-flat">Agree</a>
         </div>
     </div>
 </div>
@@ -27,7 +28,11 @@
 export default {
     name: 'Modals',
     mounted(){
-         $('.modal').modal
+        var modals = document.querySelectorAll('.modal');
+        
+        for (var i = 0; i < modals.length; i++){
+            window.M.Modal.init(modals[i]);
+        }
     }
 
 }
